83e08601841bb6add7b0f435bdd870380f9b0ebf
Sergey Goncharuk <fleshed914@gmail.com>
2021-12-03 19:58:38 +0300

    feat: add tabs to .menu section

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,3 +23,24 @@ var slider = tns({
         }
       }
   });
+
+
+const tabs = document.querySelector(".menu__container");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
+
+tabs.onclick = e => {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach(btn => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    contents.forEach(content => {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+}
